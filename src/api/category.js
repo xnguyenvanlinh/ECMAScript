ae19dca7b0504074d7b3520f997ed1f4784849a8
xnguyenvanlinh <xnguyenvanlinh@gmail.com>
2022-02-06 21:16:43 +0700

    update api

---
+++ src/api/category.js
@@ -0,0 +1,21 @@
+import instance from "./instance";
+export const getAllCate = () => {
+    const url = `/categories/`;
+    return instance.get(url);
+}
+export const getCate = (id) => {
+    const url = `/categories/${id}`;
+    return instance.get(url);
+}
+export const updateCate = (post) => {
+    const url = `/categories/${post.id}`;
+    return instance.put(url, post);
+}
+export const addCate = (post) => {
+    const url = `/categories/`;
+    return instance.post(url, post);
+}
+export const removeCate = (id) => {
+    const url = `/categories/${id}`;
+    return instance.delete(url);
+}
--- src/api/product.js
+++ src/api/product.js
@@ -1,21 +1,21 @@
 import instance from "./instance";
 export const getAll = () => {
-    const url = `/posts/`;
+    const url = `/products/`;
     return instance.get(url);
 }
 export const get = (id) => {
-    const url = `/posts/${id}`;
+    const url = `/products/${id}`;
     return instance.get(url);
 }
 export const update = (post) => {
-    const url = `/posts/${post.id}`;
+    const url = `/products/${post.id}`;
     return instance.put(url, post);
 }
 export const add = (post) => {
-    const url = `/posts/`;
+    const url = `/products/`;
     return instance.post(url, post);
 }
 export const remove = (id) => {
-    const url = `/posts/${id}`;
+    const url = `/products/${id}`;
     return instance.delete(url);
 }
