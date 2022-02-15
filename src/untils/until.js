80e7577d10ceff223a2f1d902abcf7f4a30ac3cd
xnguyenvanlinh <xnguyenvanlinh@gmail.com>
2022-02-15 17:33:23 +0700

    create action addtocart

--- src/untils/until.js
+++ src/untils/until.js
@@ -8,3 +8,53 @@ export const formatter = new Intl.NumberFormat("vi-VN", {
 });
 export const $ = document.querySelector.bind(document);
 export const $$ = document.querySelectorAll.bind(document);
+export const setUser = (user) => {
+    return localStorage.setItem('user', JSON.stringify(user))
+}
+export const checkUser = () => {
+    if (localStorage.getItem('user')) {
+        return JSON.parse(localStorage.getItem('user'))
+    } else {
+        return false
+    }
+}
+export const nextImage = (image) => {
+    const btnsPrevImage = $$('.arrow-left');
+    const btnsNextImage = $$('.arrow-right');
+    const ImageProduct = $$('.ImageProduct');
+    const countImage = $$('.count-image');
+    btnsNextImage.forEach((btn, indexBtn) => {
+        btn.addEventListener('click', () => {
+            var currentImage = Number(ImageProduct[indexBtn].dataset.index);
+            const lengthImage = image[indexBtn].images.length;
+            currentImage++;
+            if (currentImage > lengthImage - 1) {
+                currentImage = 0;
+                ImageProduct[indexBtn].src = image[indexBtn].images[currentImage];
+                ImageProduct[indexBtn].dataset.index = currentImage;
+                countImage[indexBtn].innerHTML = `${currentImage + 1}/${lengthImage}`;
+            } else {
+                ImageProduct[indexBtn].src = image[indexBtn].images[currentImage];
+                ImageProduct[indexBtn].dataset.index = currentImage;
+                countImage[indexBtn].innerHTML = `${currentImage + 1}/${lengthImage}`;
+            }
+        });
+    });
+    btnsPrevImage.forEach((btn, indexBtn) => {
+        btn.addEventListener('click', () => {
+            var currentImage = Number(ImageProduct[indexBtn].dataset.index);
+            const lengthImage = image[indexBtn].images.length;
+            currentImage--;
+            if (currentImage < 0) {
+                currentImage = lengthImage - 1;
+                ImageProduct[indexBtn].src = image[indexBtn].images[currentImage];
+                ImageProduct[indexBtn].dataset.index = currentImage;
+                countImage[indexBtn].innerHTML = `${currentImage + 1}/${lengthImage}`;
+            } else {
+                ImageProduct[indexBtn].src = image[indexBtn].images[currentImage];
+                ImageProduct[indexBtn].dataset.index = currentImage;
+                countImage[indexBtn].innerHTML = `${currentImage + 1}/${lengthImage}`;
+            }
+        });
+    });
+}
