909d2690534aca7ce91d6b5e6846f53144d8ff4c
xnguyenvanlinh <xnguyenvanlinh@gmail.com>
2022-02-21 15:08:09 +0700

    feat : create show cart

--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -1,51 +1,72 @@
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
-import { formatter } from '../../untils/until';
-
-/* eslint-disable indent */
+import {
+    increaseQuantity,
+    decreaseQuantity,
+    removeItemInCart,
+    removeCart,
+} from '../../untils/addtocart';
+import { $, $$, formatter, reRender } from '../../untils/until';
+import toastr from 'toastr';
+import 'toastr/build/toastr.min.css';
+import { addCart } from '../../api/carts';
 const Cart = {
-        render() {
-            const carts = JSON.parse(localStorage.getItem('cart'));
-
-            console.log(carts)
-            return /* html */ `
+    render() {
+        const carts = JSON.parse(localStorage.getItem('cart'));
+        return /* html */ `
             ${Header.render()}
+            ${carts ?/* html */`
             <div class="container mx-auto mt-10">
                 <div class="flex shadow-md my-10">
                 <div class="w-3/4 bg-white px-10 py-10">
                     <div class="flex justify-between border-b pb-8">
                     <h1 class="font-semibold text-2xl">Giỏ hàng</h1>
-                    <h2 class="font-semibold text-2xl">3 Sản phẩm</h2>
+                    <h2 class="font-semibold text-2xl">${carts.length
+                } Sản phẩm</h2>
                     </div>
                     <div class="flex mt-10 mb-5">
                     <h3 class="font-semibold text-gray-600 text-xs uppercase w-2/5">Product Details</h3>
                     <h3 class="font-semibold text-gray-600 text-xs uppercase w-1/5 text-center">Số lượng</h3>
                     <h3 class="font-semibold text-gray-600 text-xs uppercase w-1/5 text-center">Đơn giá</h3>
-                    <h3 class="font-semibold text-gray-600 text-xs uppercase w-1/5 text-center">Tổng tiền</h3>
+                    <h3 class="font-semibold text-gray-600 text-xs uppercase w-1/5 text-center">Thành tiền</h3>
                     </div>
-                    ${carts.map((cart) => /* html */`
+                    ${carts
+                    .map(
+                        (cart) => /* html */ `
                     <div class="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
                         <div class="flex w-2/5"> 
                             <div class="w-20 h-20">
-                            <img class="object-cover" style="width:100px;height:100px;" src="${cart.images[0]}" alt="">
+                            <img class="object-cover" style="width:100px;height:100px;" src="${cart.images[0]
+                            }" alt="">
                             </div>
                             <div class="flex flex-col justify-between ml-4 flex-grow">
                             <span class="font-bold text-sm">${cart.name}</span>
-                            <button id="remove-product" class="remove w-fit btn" data-id=${cart.id}>Xóa</button>
+                            <button id="remove-product" class="remove w-fit btn" data-id=${cart.id
+                            }>Xóa</button>
                             </div>
                         </div>
                         <div class="flex justify-center w-1/5">
-                            <svg id="descrease" class="cursor-pointer descrease btn fill-current text-gray-600 w-3" viewBox="0 0 448 512"><path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" data-id="${cart.id}"/>
-                            </svg>
-                            <input class="mx-2 border text-center w-8" type="text" value=${cart.quantity}>
-                            <svg id="descrease" class="cursor-pointer descrease fill-current text-gray-600 w-3 btn" viewBox="0 0 448 512" data-id="${cart.id}">
-                            <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
-                            </svg>
+                            <button class="btn decrease" data-id="${cart.id}">
+                                <svg id="decrease" class="cursor-pointer fill-current text-gray-600 w-3" viewBox="0 0 448 512"><path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
+                                </svg>
+                            </button>
+                            <input class="mx-2 border text-center w-8" type="text" value=${cart.quantity
+                            }>
+                            <button class="btn increase" data-id="${cart.id}">
+                                <svg id="inscrease" class="cursor-pointer fill-current text-gray-600 w-3" viewBox="0 0 448 512" >
+                                <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
+                                </svg>
+                            </button>
                         </div>
-                        <span class="text-center w-1/5 font-semibold text-sm">${formatter.format(cart.price)}</span>
-                        <span class="text-center w-1/5 font-semibold text-sm">${formatter.format(cart.price*cart.quantity)}</span>
+                        <span class="text-center w-1/5 font-semibold text-sm">
+                        ${formatter.format(cart.price)}
+                        </span>
+                        <span class="text-center w-1/5 font-semibold text-sm">
+                        ${formatter.format(cart.price * cart.quantity)}</span>
                     </div>
-                    `).join('')}
+                    `
+                    )
+                    .join('')}
                     <a href="#" class="flex font-semibold text-indigo-600 text-sm mt-10">
                     <svg class="fill-current mr-2 text-indigo-600 w-4" viewBox="0 0 448 512"><path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z"/></svg>
                     Tiếp tục mua hàng
@@ -55,40 +76,64 @@ const Cart = {
                 <div id="summary" class="w-1/4 px-8 py-10">
                     <h1 class="font-semibold text-2xl border-b pb-8">Thanh Toán</h1>
                     <div class="flex font-semibold justify-between py-6 text-sm uppercase">
-                        <span>Total cost</span>
-                        <span>$600</span>
+                        <span>Tổng tiền</span>
+                        <span id="total"></span>
                     </div>
-                    <button class="bg-indigo-500 font-semibold hover:bg-indigo-600 py-3 text-sm text-white uppercase w-full">Đặt hàng</button>
+                    <button class="order bg-indigo-500 font-semibold hover:bg-indigo-600 py-3 text-sm text-white uppercase w-full">Đặt hàng</button>
                     </div>
                 </div>
                 </div>
             </div>
+            `:/* html */`
+                <h2 class="font-semibold text-4xl mx-auto w-fit my-5">Giỏ hàng của bạn đang trống</h2>       
+            `}
             ${Footer.render()}
         `;
     },
     afterRender() {
-        Footer.afterRender()
-        const btns = document.querySelectorAll('.btn')
-        console.log(btns)
-        btns.forEach(btn => {
-            btn.addEventListener('click', function(){
-                const id = btn.dataset.id;
-                if(btn.classList.contains('increase')){
-                    console.log('increase')
-                    // increaseQuantity(id, () => {
-                        // toastr.success("Tăng số lượng thành công")
-                    // })
-                } else if(btn.classList.contains('decrease')){
-                    console.log('decrease')
-
-                    // decreaseQuantity(id);
-                } else {
-                    // removeItemInCart(id, () => {
-                        // reRender(CartPage, "#content");
-                    // });
-                }
+        Footer.afterRender();
+        const showTotal = $('#total');
+        let total = 0;
+        const cart = JSON.parse(localStorage.getItem('cart'));
+        if (cart) {
+            for (let i = 0; i < cart.length; i++) {
+                total += Number(cart[i].quantity) * Number(cart[i].price);
+            }
+        }
+        const order = $('.order');
+        if (order) {
+            order.addEventListener('click', async () => {
+                toastr.success('Đặt hàng thành công');
+                await addCart({
+                    email: JSON.parse(localStorage.getItem('user')).email,
+                    cart: JSON.parse(localStorage.getItem('cart')),
+                }).then(() => {
+                    removeCart()
+                })
             })
+        }
+        if (showTotal) {
+            showTotal.innerHTML = formatter.format(total);
+        }
+        const btns = $$('.btn');
+        btns.forEach((btn) => {
+            btn.addEventListener('click', function () {
+                const id = btn.dataset.id;
+                if (btn.classList.contains('increase')) {
+                    increaseQuantity(id, () => {
+                        toastr.success('Tăng số lượng thành công');
+                    });
+                    reRender(Cart, '#root');
+                } else if (btn.classList.contains('decrease')) {
+                    decreaseQuantity(id);
+                    reRender(Cart, '#root');
+                } else {
+                    removeItemInCart(id, () => {
+                        reRender(Cart, '#content');
+                    });
+                }
+            });
         });
-    }
+    },
 };
 export default Cart;
