494639f9ec6d665f2ad9cb2828b4067625f2c4a3
xnguyenvanlinh <xnguyenvanlinh@gmail.com>
2022-02-21 15:07:26 +0700

    refactor : re import

--- src/main.js
+++ src/main.js
@@ -1,13 +1,20 @@
 import Navigo from 'navigo';
 import DetailProduct from './pages/DetailProduct';
-import Addproduct from './pages/admin/AddProduct.js';
-import EditProduct from './pages/admin/EditProduct';
-import Products from './pages/admin/Products';
+import Addproduct from './pages/admin/product/AddProduct.js';
+import EditProduct from './pages/admin/product/EditProduct';
+import Products from './pages/admin/product/Products';
 import Categories from './pages/category';
 import HomePage from './pages/HomePage';
 import Login from './pages/user/login';
 import Signup from './pages/user/signup';
 import Cart from './pages/cart';
+import Category from './pages/admin/category';
+import AddCateGory from './pages/admin/category/addCategory';
+import EditCategory from './pages/admin/category/edit';
+import pageProduct from './pages/products';
+import Users from './pages/admin/user';
+import EditUser from './pages/admin/user/edit';
+import ListCart from './pages/admin/cart';
 const router = new Navigo('/', { hash: true, linksSelector: 'a' });
 const $ = document.querySelector.bind(document);
 const print = async(content, id) => {
@@ -36,14 +43,21 @@ router.on({
     '/': () => print(HomePage),
     '/signup': () => print(Signup),
     '/signin': () => print(Login),
+    '/products': () => print(pageProduct),
     '/cart': () => print(Cart),
     '/category/:id': ({ data }) => print(Categories, data.id),
     '/products/:id': ({ data }) => print(DetailProduct, data.id),
-    '/information': () => print(console.log('information')),
+    '/information': () => console.log('information'),
     '/admin': () => print(Products),
+    '/admin/users': () => print(Users),
+    '/admin/users/:id/edit': ({data}) => print(EditUser,data.id),
+    '/admin/category': () => print(Category),
+    '/admin/category/add': () => print(AddCateGory),
+    '/admin/category/:id/edit': ({ data }) => print(EditCategory, data.id),
     '/admin/products': () => print(Products),
     '/admin/products/add': () => print(Addproduct),
     '/admin/product/:id/edit': ({ data }) => print(EditProduct, data.id),
+    '/admin/revenue': () => print(ListCart),
 
 });
 router.notFound(() => {});
