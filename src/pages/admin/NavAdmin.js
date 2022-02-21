1e962174801b1e83fa9eb6f7988f8eae2657adcd
xnguyenvanlinh <xnguyenvanlinh@gmail.com>
2022-02-21 18:07:26 +0700

    refactor : change Logo

--- src/pages/admin/NavAdmin.js
+++ src/pages/admin/NavAdmin.js
@@ -17,8 +17,7 @@ const NavAdmin = {
                         </div>
                         <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                         <a href="/#/" class="flex-shrink-0 flex items-center">
-                            <img class="block lg:hidden h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow">
-                            <img class="hidden lg:block h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow">
+                            <img class="block h-8 w-auto" src="../../../logo.png" alt="Workflow">
                         </a>
                         <div class="hidden sm:block sm:ml-6">
                             <div class="flex space-x-4">
